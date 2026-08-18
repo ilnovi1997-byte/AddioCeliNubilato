492aa1787df27970aaae28a24af6337ebfd29060
ilnovi1997-byte <ilnovi1997@gmail.com>
2026-08-18 09:45:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -66,4 +66,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server attivo su http://localhost:${PORT}`));
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server attivo sulla porta ${PORT}`);
+});
